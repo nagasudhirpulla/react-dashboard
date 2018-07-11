17a0ca905454de190c3b815c588ddba2ee4e0842
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-07-11 16:23:47 +0530

    fileserver config feature done

--- sample_csv_server/app.js
+++ sample_csv_server/app.js
@@ -1,33 +1,26 @@
-/*
-http://stackabuse.com/node-http-servers-for-static-file-serving/
-*/
-
-/*
-var http = require('http');
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
-var staticBasePath = './csv_files';
-
-var serve = serveStatic(staticBasePath, { 'index': false });
-
-var server = http.createServer(function (req, res) {
-    var done = finalhandler(req, res);
-    serve(req, res, done);
-})
-
-server.listen(8807);
-*/
-
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 var cors = require('cors');
+var fs = require('fs');
 
 var app = express();
 app.use(cors());
 
+// default serve path
 app.use(serveStatic(path.join(__dirname, 'csv_files')));
+
+// read the config json file for path settings
+var configObj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var pathsDict = configObj.paths;
+var routeStrs = Object.keys(pathsDict);
+
+for (let routeStrIter = 0; routeStrIter < routeStrs.length; routeStrIter++) {
+    // make the app to use routes as per the setting
+    var routeStr = routeStrs[routeStrIter];
+    app.use(routeStr, serveStatic(pathsDict[routeStr]));
+}
+
 // app.use('/sudhir', serveStatic(path.join(__dirname, 'csv_files')));
-//app.use(serveStatic(path.join(__dirname, 'public')));
+
 app.listen(8807, () => console.log('Example app listening on port 8807!'))
