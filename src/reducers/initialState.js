049553fc2db2686cfd17dfc80d42c49958ab05cb
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-09-09 21:20:00 +0530

    changed init state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -4,29 +4,7 @@ https://github.com/SophieDeBenedetto/catbook-redux/blob/master/src/reducers/init
 export default {
     dashboard: {
         dashboard_name: 'Dashboard Web App',
-        dashboard_cells: [
-            {
-                cell_name: 'First Cell',
-                cell_type: 'csv_plot',
-                cell_geometry: {
-                    cell_col_str: 'col-sm-6',
-                    cell_min_width: '0px',
-                    cell_min_height: '100px'
-                },
-                csv_plot_props: {
-                    plot_modes: ['lines', 'lines'],
-                    x_headings: ['x', 'x'],
-                    y_headings: ['y', 'z'],
-                    csv_address: 'http://localhost:8807/sample_1.csv'
-                },
-                auto_fetch:{
-                    enabled: false,
-                    fetch_mins: 0,
-                    fetch_secs: 0
-                },
-                data: {}
-            }
-        ]
+        dashboard_cells: []
     },
     dashbard_cell: {
         cell_name: 'Dashboard Cell',
